f8e938ea402f8e761b7b2d2bd4f0ef263f4f16a3
maoyiwei <jsmyw95@qq.com>
2018-10-27 19:03:06 +0800

    primary Product

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -1,3 +1,90 @@
-export default () => {
-  return <h1>Workplace Page</h1>
-};
+import React, { Component } from 'react';
+import { Table, Button, Input } from 'antd'
+import _ from '../../util/util'
+
+import styles from './index.less'
+
+export default class Product extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      dataFilter: this.dataSource
+    }
+
+    this.onSearch = this.onSearch.bind(this);
+  }
+
+  columns = [{
+    title: '基金名称',
+    dataIndex: 'name',
+    key: 'name'
+  }, {
+    title: '近七日年化',
+    dataIndex: 'dividend',
+    key: 'dividend',
+    sorter: (a, b) => a.dividend - b.dividend,
+    render: (text) => {
+      return <span className="red">{
+        text + "%"
+      }</span>
+    }
+  }, {
+    title: '万份收益（元）',
+    dataIndex: 'gains',
+    key: 'gains',
+    sorter: (a, b) => a.gains - b.gains,
+  }, {
+    title: '期限',
+    dataIndex: 'expireDate',
+    key: 'expireDate'
+  }, {
+    title: '',
+    dataIndex: 'action',
+    key: 'action',
+    render: (text, record) => {
+      return <Button type="primary">
+        买入
+      </Button>
+    }
+  }]
+
+  dataSource = [{
+    key: 1,
+    name: "华夏财富宝",
+    dividend: 3.0560,
+    gains: 0.9465,
+    expireDate: "随买随取"
+  }, {
+    key: 2,
+    name: "天弘财富宝",
+    dividend: 3.721,
+    gains: 0.9777,
+    expireDate: "随买随取"
+  }]
+
+
+  onSearch(event) {
+    const text = event.target.value;
+    this.setState({
+      dataFilter: _.searchText(this.dataSource, text, "name")
+    })
+  }
+
+  render() {
+    return <div>
+      <div className={styles.top}>
+        <Input
+          onChange={this.onSearch}
+          className={styles.searchBox}
+          placeholder="请输入基金名"
+        />
+      </div>
+      <Table
+        columns={this.columns}
+        dataSource={this.state.dataFilter}
+      />
+    </div>
+  }
+}
---
+++ src/util/util.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+
+export default {
+    /**
+         * 在record所有字段中搜索并返回filter数组
+         * 
+         * @param {any} originList 
+         * @param {any} text 
+         */
+    searchText(originList, text, columnList) {
+        let filter = [];
+
+        if (Object.prototype.toString.call(originList) === "[object Array]") {
+            let reg = new RegExp(text, 'gi');
+
+            // 遍历所有原始数据
+            originList.map((record, index) => {
+                let match, passFlag = false, newRecord = {};
+                for (let k in record) {
+                    if (
+                        columnList && columnList.indexOf(k) < 0
+                        || !(typeof (record[k]) === "string")
+                    ) {
+
+                        newRecord[k] = record[k];
+                        continue;
+                    }
+                    match = record[k].match(reg);
+                    if (match) {
+                        passFlag = true;
+                        newRecord[k] = (
+                            <span>
+                                {
+                                    record[k].split(reg).map((text, i) => (
+                                        i > 0 ? [<span key={i} className="hignlight">
+                                            {match[0]}
+                                        </span>, text] : text
+                                    ))
+                                }
+                            </span>
+                        );
+                    } else {
+                        newRecord[k] = record[k];
+                    }
+                }
+                if (passFlag) {
+                    filter.push(newRecord);
+                }
+            })
+        }
+        return filter;
+    },
+}
