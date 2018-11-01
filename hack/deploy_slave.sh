a8d668589382b46f8f917078ef808ba5a7bac230
Junjun Li <junjunli666@gmail.com>
2018-11-01 15:18:28 +0800

    add dir to manage deployment

--- hack/deploy_slave.sh
+++ hack/deploy_slave.sh
@@ -2,11 +2,9 @@
 git pull
 
 # build the new image
-docker build . -t front:v1
+# docker build  . -t front:v1
+docker build -f ../Dockerfile . -t front:v1
 
 # delete the old image
 # todo: delete runing cantainer
 docker images|grep none|awk '{print $3}'|xargs docker rmi
-
-# run container
-docker run -d -p 8888:8888 front:v1
